852250c31ffdc124811fe39c4853872cc3592a5f
古月余空 <62677831+narst@users.noreply.github.com>
2025-09-26 22:54:03 +0800

    Add files via upload

---
+++ 4.8.1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char ming[32], xin[32];  
+    printf("请输入你的名和姓（用空格分隔）：");
+    scanf("%s %s",ming,xin);   
+    printf("%s %s\n", ming,xin);
+    return 0;
+}
+
